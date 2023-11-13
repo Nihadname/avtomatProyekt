04b67c778c1bd4fcfd2719518b7a8cfcbad0c534
Nihadname <nihadcoding@gmail.com>
2023-11-13 20:20:16 +0400

    Add project files.

---
+++ avtomatProyekt/Models/Avtomat.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avtomatProyekt.Models
+{
+    internal class Avtomat
+    {
+     public   string Ad;
+       public int UmumiGulleSayi;
+        public int ElimezdeQalanGulleSayi;
+
+        public Avtomat(string ad, int umumiGulleSayi)
+        {
+            Ad = ad;
+            UmumiGulleSayi = umumiGulleSayi;
+            ElimezdeQalanGulleSayi = umumiGulleSayi;
+        }
+        public void BirGulle()
+        {
+           
+            if(ElimezdeQalanGulleSayi>0) {
+                Console.WriteLine("1 gulle atilir");
+                ElimezdeQalanGulleSayi--;
+                QalanGulleSayiniGostermek();
+
+            }
+            else
+            {
+                GulleninDoldurulmasi();
+               
+               
+            }
+            
+        }
+        public void AvtomatikEtas()
+        {
+            int HerAvtomatikGulleEtasSayisi = 3;
+            if (HerAvtomatikGulleEtasSayisi >= ElimezdeQalanGulleSayi)
+            {
+                Console.WriteLine("avtomatik etas baslamisdir");
+                ElimezdeQalanGulleSayi -= HerAvtomatikGulleEtasSayisi;
+                QalanGulleSayiniGostermek();
+            }
+            else
+            {
+                Console.WriteLine("gullemiz qurtarib");
+            }
+        }
+        public void GulleninDoldurulmasi()
+        {
+            if (ElimezdeQalanGulleSayi == 0)
+            {
+                Console.WriteLine("gulle doldurulur");
+                ElimezdeQalanGulleSayi = UmumiGulleSayi;
+                QalanGulleSayiniGostermek() ;
+            }
+        }
+        public void QalanGulleSayiniGostermek()
+        {
+            Console.WriteLine($"{ElimezdeQalanGulleSayi}/{UmumiGulleSayi}");
+        }
+
+    }
+}
---
+++ avtomatProyekt/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using avtomatProyekt.Models;
+
+Console.WriteLine("Hello, World!");
+
+Avtomat avtomat = new Avtomat("Ak-47",30);
+char OyunMeqsediniBelirleyen=char.Parse(Console.ReadLine());
+
+Console.WriteLine("oyun baslayacaq amma optionlariniz olacaq birini secin");
+Console.WriteLine("1-tek-tek ates");
+Console.WriteLine("2-Avtomatik modda etas");
+Console.WriteLine("3-daragin doldurulmasi");
+while (true)
+{
+    switch (OyunMeqsediniBelirleyen) {
+        case '1':
+            avtomat.BirGulle();
+            break;
+        case '2':
+            avtomat.AvtomatikEtas();
+            break;
+        case '3':
+            avtomat.GulleninDoldurulmasi();
+            break;
+            default:
+            Console.WriteLine("oyun baslamaq ucun duzgun reqem secin");
+             OyunMeqsediniBelirleyen = char.Parse(Console.ReadLine());
+            break;  
+
+    
+    }
+
+}
